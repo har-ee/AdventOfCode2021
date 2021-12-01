d41ec3ee949d92a0ca67066f8bb6ff7e3941e4bd
Harry Everett <github@har.ee>
2021-12-01 22:36:02 +0000

    Add Day1B solution

---
+++ day1/day1b.hs
@@ -0,0 +1,8 @@
+main :: IO ()
+main = do
+  input <- readFile "day1/input"
+  let sonar = map (read :: String -> Int) (lines input)
+
+  let window = zipWith3 (\x y z -> x + y + z) sonar (tail sonar) (tail $ tail sonar)
+  let answer = length $ filter id $zipWith (<) window (tail window)
+  print answer

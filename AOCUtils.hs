51bbd03d614e6b6f01a87ea86196f35b72baf841
Harry Everett <github@har.ee>
2021-12-02 22:14:52 +0000

    Add utils file

---
+++ AOCUtils.hs
@@ -0,0 +1,9 @@
+module AOCUtils where
+import Text.Regex.TDFA ((=~))
+
+-- Splits string based on regex
+split :: String -> String -> [String]
+split [] _ = []
+split str regex = before : split upcoming regex
+  where
+    (before, _, upcoming) = str =~ regex :: (String, String, String)

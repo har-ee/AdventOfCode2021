df5782a68c85f6be152cca640d92f2c12bab0c73
Harry Everett <github@har.ee>
2021-12-06 13:40:06 +0000

    Add day 6 solution
    Quite nice in haskell!

---
+++ day6/day6a.hs
@@ -0,0 +1,13 @@
+import AOCUtils (frequency)
+import Data.List (group, sort)
+
+main :: IO ()
+main = do
+  input <- readFile "day6/input"
+  let i = read ("[" ++ input ++ "]") :: [Int]
+  let frequencies = map (`frequency` i) [0..8] 
+
+  print $ sum $ iterate simulate frequencies !! 80
+
+simulate :: [Int] -> [Int]
+simulate (a0:a1:a2:a3:a4:a5:a6:a7:a8:xs) = a1:a2:a3:a4:a5:a6:(a7 + a0):a8:a0:xs
